e83d559b17a94e018df7532ceda190477989d6b7
Rahul Panjiyara <rahulpanjiyara@gmail.com>
2025-08-16 14:58:58 +0530

    Refined some tweaks

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
+
 export default function Preview() {
   const location = useLocation();
   const navigate = useNavigate();
@@ -11,20 +12,22 @@ export default function Preview() {
   // If no content, redirect back to form or show error
   if (!htmlContent) {
     return (
-      <div>
-        <p>No preview available.</p>
-        <button onClick={() => navigate(-1)}>Go Back</button>
+      <div className="flex flex-col justify-center items-center min-h-screen">
+        <h1 className="text-2xl mb-3">No Preview Available</h1>
+        <button className="btn btn-secondary" onClick={() => navigate(-1)}>
+          Go Back
+        </button>
       </div>
     );
   }
 
   return (
     <div>
+      
       <div
         // style={{ padding: "2rem", marginTop: "1rem" }}
         dangerouslySetInnerHTML={{ __html: htmlContent }}
       />
-      
     </div>
   );
 }
